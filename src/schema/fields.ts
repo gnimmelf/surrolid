0bc9bc66e86095eef678ab3c6ce8ece54b9452c5
Flemming Hansen <flemming.hansen@noria.no>
2023-03-02 21:35:45 +0100

    schema fields defs

--- src/schema/fields.ts
+++ src/schema/fields.ts
@@ -1,12 +1,38 @@
-import { z } from 'zod';
+import { Setter } from 'solid-js';
+import { z, ZodSchema } from 'zod';
+
+const reName = new RegExp(/^[\p{L}'][ \p{L}'-]*[\p{L}]$/u);
 
 export const email = z.string().email('Must be a valid email address');
-
-export const pass = z
+export const name = z
   .string()
-  .min(3, 'Minimum 3 charcters, must contain both letters and numbers');
+  .trim()
+  .min(2, 'Minimum 2 charcters')
+  .regex(reName, 'Must be a vaild name');
 
-export const firstName = z.string().min(2);
-export const lastName = z.string().min(2);
-export const address = z.string().min(3).or(z.literal(''));
-export const phone = z.string().min(8).or(z.literal(''));
+export const pass = z.string().trim().min(3, 'Minimum 3 charcters');
+
+export const address = z.string().trim().min(3).or(z.literal(''));
+export const phone = z
+  .string()
+  .trim()
+  .min(8, 'Minimum 8 charcters')
+  .or(z.literal(''));
+
+export const validateValues = (
+  Schema: ZodSchema,
+  values: z.infer<typeof Schema>,
+  setErrors: Setter<{
+    formErrors: string[];
+    fieldErrors: { [x: string]: string[] };
+  }>
+) => {
+  const res = Schema.safeParse(values);
+  if (res.success) {
+    setErrors({});
+    return res.data;
+  } else {
+    // Remember to flatten!setErrors
+    setErrors(res.error.flatten());
+  }
+};
